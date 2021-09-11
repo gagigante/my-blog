440f4a2153bfe624a2dafd89767af7e4785b934b
Gabriel Gigante <gabriel_gigante@outlook.com>
2021-09-11 19:26:32 -0300

    feat: adding header component to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
+import { Header } from '@components/Header'
+
+import '@styles/globals.scss'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
+
 export default MyApp
