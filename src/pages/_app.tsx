3290ce74f065f0d463c00a1a0f7ab9a2a14bf284
Gabriel Gigante <gabrielgigante29@gmail.com>
2021-11-16 13:44:00 -0300

    chore: adjusting global styles imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
 
 import '@styles/globals.scss'
+import 'react-activity/dist/Dots.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
