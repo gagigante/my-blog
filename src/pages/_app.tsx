e983e4ce85700f0f22a488410bad0ecbe2b05334
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2024-04-25 15:17:11 -0300

    [FEAT] adds vercel analytics (#4)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Raleway as ralaway, JetBrains_Mono as jetbrains } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 import '@styles/globals.scss'
 import 'react-activity/dist/Dots.css'
@@ -18,6 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={`${primary.className} ${secondary.className} font-sans`}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   )
 }
