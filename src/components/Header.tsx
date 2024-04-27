ec11e48e82f77687b9459a8ad6fddbad40498ec4
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2024-04-27 12:24:47 -0300

    Revert "feat: alteracao text" (#6)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export const Header = ({ githubUrl, linkedInUrl, twitterUrl }: HeaderProps) => {
   return (
     <header className={styles.wrapper}>
       <div className={styles.content}>
-        <Link href="/">heloisa.fedo</Link>
+        <Link href="/">gg.dev</Link>
 
         <ul className={styles.links}>
           {githubUrl !== 'null' && (
