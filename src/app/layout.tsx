9ba0a3658a95b03d2ce04853d17ebc361db72850
Gabriel Gigante <gabriel.gigante@isaac.com.br>
2024-04-29 19:08:29 -0300

    fix: fixes header links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,15 @@ import { Analytics } from '@vercel/analytics/react'
 
 import { Header } from '@components/Header'
 
+import { getSocialLinks } from './utils'
+
 import '@styles/globals.scss'
 import styles from './layout.module.scss'
 import 'react-activity/dist/Dots.css'
 
 export const metadata = {
-  title: 'Blog | Gabriel Gigante'
-  // TODO: description: ''
+  title: 'Blog | Gabriel Gigante',
+  description: 'Apenas um desenvolvedor de software que gosta de compartilhar conhecimento'
 }
 
 const primary = ralaway({
@@ -23,15 +25,18 @@ const secondary = jetbrains({
   subsets: ['latin']
 })
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const revalidate = 3600 // 1 hour
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const { githubUrl, linkedInUrl, twitterUrl } = await getSocialLinks()
   return (
     <html lang="pt-BR" className={`${primary.className} ${secondary.className}`}>
       <body>
-        <Header githubUrl={'githubUrl'} linkedInUrl={'linkedInUrl'} twitterUrl={'null'} />
+        <Header githubUrl={githubUrl} linkedInUrl={linkedInUrl} twitterUrl={twitterUrl} />
         <div className={styles.content}>{children}</div>
       </body>
       <Analytics />
-      {/* <SpeedInsights /> */}
+      {/* TODO: <SpeedInsights /> */}
     </html>
   )
 }
